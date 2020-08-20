155e691c5f1b7eaa480109809fb26933319ce0d3
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-20 15:13:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  add_element_to_end_of_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  add_element_to_start_of_array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
