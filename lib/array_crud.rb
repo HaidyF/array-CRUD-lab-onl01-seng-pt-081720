3d1a1f6c75058ea3de2fce75477c852249af12d2
Haidy  Farahat <haidyfarahat@gmail.com>
2020-08-20 15:24:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,9 +33,10 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.index(0)
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[-1]
 end
